5f99ef948d8e9cd15690e1105cd897128874f3a4
Skilldrick <skilldrick@gmail.com>
2011-03-19 18:53:09 +0000

    Added stub test.

--- ajax/test/ajax_test.js
+++ ajax/test/ajax_test.js
@@ -6,5 +6,19 @@ TestCase("AjaxCreateTest", {
     assert(tddjs.isHostMethod(xhr, "open"));
     assert(tddjs.isHostMethod(xhr, "send"));
     assert(tddjs.isHostMethod(xhr, "setRequestHeader"));
-  }
+  },
+
+  "test should obtain an XMLHttpRequest object": function () {
+    var originalCreate = ajax.create;
+
+    ajax.create = function () {
+      ajax.create.called = true;
+    };
+
+    ajax.get("/url");
+
+    assert(ajax.create.called);
+
+    ajax.create = originalCreate;
+  },
 });
